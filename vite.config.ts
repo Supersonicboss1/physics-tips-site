6d9c100c10d9080451cdc30932ed7609831c0399
Supersonicboss1 <leodalgado@gmail.com>
2023-01-26 23:47:30 +0000

    wsl2 fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		watch: {
+			// This is required for WSL2
+			usePolling: true,
+		},
+	},
 };
 
 export default config;
