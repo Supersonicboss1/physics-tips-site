4a568daa9d0c6941eb06c2ff3858a6f39ef1bece
Supersonicboss1 <leodalgado@gmail.com>
2023-01-26 23:51:18 +0000

    additional complaining

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,8 @@
 
-<div>
-<h1 class="">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex justify-center">
+    <div class="w-1/2">
+        <h1 class="text-4xl font-bold">Tips</h1>
+        <p class="text-gray-600">Here are some tips for you</p>
+        If you are making a website, don't do maths, it is annoying to make work in the browser properly. Use images if you have to 👍
+    </div>
 </div>
