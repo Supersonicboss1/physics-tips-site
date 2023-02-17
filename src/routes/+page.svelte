38f0607aae9b728803af7e0897e983b6683405a2
Supersonicboss1 <leodalgado@gmail.com>
2023-02-17 19:27:51 +0000

    change home text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <div class="flex justify-center">
     <div class="w-1/2">
-        <h1 class="text-4xl font-bold">Tips</h1>
-        <p class="text-gray-600">Here are some tips for you</p>
-        If you are making a website, don't do maths, it is annoying to make work
-        in the browser properly. Use images if you have to 👍
+        <h1 class="text-4xl font-bold">bold</h1>
+        <p class="text-gray-600">i stole the osu font</p>
+
     </div>
 </div>
